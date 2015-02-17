0342ea341508205dbef4e4dfd5f7630e28e8d14c
krpbrown <krpbrown@clarkson.edu>
2015-02-17 12:32:15 -0500

    Initial shape
    Drew initial shape in preparation to move it.

--- lab2.js
+++ lab2.js
@@ -1,22 +1,48 @@
+// Kristian Brown 2/17/15 Lab 2
 var gl;
 var points;
+
+var vPosition;
+var bufferId;
+var program;
+var vertices = new Float32Array([-.5, .5, .5, .5, 0.25, 0, -0.25, 0]);	//trapezoid
+
 window.onload = function init()
 {
-var canvas = document.getElementById( "gl-canvas" );
-gl = WebGLUtils.setupWebGL( canvas );
-if ( !gl ) { alert( "WebGL isn't available" ); }
-//
-// Configure WebGL
-//
-gl.viewport( 0, 0, canvas.width, canvas.height );
-gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-// Load shaders and initialize attribute buffers
-var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-gl.useProgram( program );
-// Load the data into the GPU
-// Associate our shader variables with our data buffer
-render();
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    // Associate our shader variables with our data buffer
+    bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW );
+     
+	
+    render();
 };
+
+
 function render() {
-gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	//triangle initial
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
 }
+
