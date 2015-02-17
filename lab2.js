d604722dab0af051bf17e5eaf44b6eb64835aaca
krpbrown <krpbrown@clarkson.edu>
2015-02-17 18:25:54 -0500

    Completed
    Added interaction

--- lab2.js
+++ lab2.js
@@ -1,12 +1,8 @@
 // Kristian Brown 2/17/15 Lab 2
 var gl;
-var points;
-
-var vPosition;
-var bufferId;
-var program;
-var vertices = new Float32Array([-.5, .5, .5, .5, 0.25, 0, -0.25, 0]);	//trapezoid
-
+var verticesBuffer;
+var translateTo;
+var translation = [0,0];
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
@@ -18,31 +14,73 @@ window.onload = function init()
     //  Configure WebGL
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
+    	
     //  Load shaders and initialize attribute buffers
     
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
-    
-    // Load the data into the GPU
-    // Associate our shader variables with our data buffer
-    bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW );
-     
+    gl.program = program;
+	var i = draw(gl)
 	
-    render();
+	translateTo = gl.getUniformLocation( gl.program, "translate");
+	//Listen for key press
+	document.onkeydown = function(k){
+			switch(String.fromCharCode(k.keyCode)){
+				case "1":	//center
+					translation = [0,0];
+				break;
+				case "A":	//move left
+					translation[0] = translation[0] - 0.2;
+				break;
+				case "S":	//move down
+					translation[1] = translation[1] - 0.2;
+				break;
+				case "D":	//move right
+					translation[0] = translation[0] + 0.2;
+				break;
+				case "W":	//move up
+					translation[1] = translation[1] + 0.2;
+				break;
+			}
+		render(i);
+	}	
+	gl.clearColor( 0.0, 0.5, 0.7, 1.0 );		//set background color
+	render(i);
 };
 
 
-function render() {
+function render(i) {
 	gl.clear( gl.COLOR_BUFFER_BIT );
-	//triangle initial
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+	gl.uniform2fv(translateTo, translation);
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, i );
 }
+
+
+function draw(gl){
+	var i = 5;	//num of vertices
+	verticesBuffer = gl.createBuffer();
+	//vertices for my pentagon, followed by the color of each vertex i.e. x,y,R,G,B
+	var vertices = new Float32Array([
+		-0.5, 0.25, 0, 0.5, 0.5,
+		0, 0.50, 0.5, 0.0, 0.5,
+		 0.5, 0.25, 0, 0.5, 0.5,
+		 0.25, -0.25, 0.5, 0.0, 0.5,
+		 -0.25, -0.25, 0.5, 0.0, 0.5 ]);
+	gl.bindBuffer( gl.ARRAY_BUFFER, verticesBuffer);
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+	
+	var size = vertices.BYTES_PER_ELEMENT;
+
+	var col = gl.getAttribLocation( gl.program, "color");
+	gl.vertexAttribPointer(col, 3, gl.FLOAT, false, size*5, size*2);
+	gl.enableVertexAttribArray(col);
+
+	var pos = gl.getAttribLocation( gl.program, "position");
+	gl.vertexAttribPointer(pos, 2, gl.FLOAT, false, size*5, 0);
+	gl.enableVertexAttribArray(pos);
+	
+	
+	return i;
+}
+
 
